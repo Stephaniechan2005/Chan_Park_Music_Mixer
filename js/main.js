52fd5f5fd855175e4a2bee2eca5fcd00adf2eab5
KyuriPark00 <156356426+KyuriPark00@users.noreply.github.com>
2024-07-24 09:08:11 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,37 +1,47 @@
 console.log("JavaScript is connected");
-//Variable
-const theButtons = document.querySelectorAll("#buttonHolder img");
 
-const puzzleBoard = document.querySelector(".puzzle-board");
-const puzzlePieces = document.querySelectorAll(".puzzle-pieces img");
+// Variables
 const dropZones = document.querySelectorAll(".drop-zone");
-const puzzlePieceDiv = document.querySelector(".puzzle-pieces");
+const instruments = document.querySelectorAll(".instruments-pieces img");
+const playButton = document.querySelector("#playButton"),
+  pauseButton = document.querySelector("#pauseButton"),
+  stopButton = document.querySelector("#stopButton"),
+  resetButton = document.querySelector("#resetButton"),
+  volSlider = document.querySelector("#volumeControl");
+
 let dragPiece;
+let playingAudios = [];
+const originalPositions = [];
 
-//function
-function changeBGImage(event) {
-  console.log("changeBGImage called");
-  //Method 1
-  //console.log(this.id);
-  //background-image: url('../images/backGround0.jpg');
-  // puzzleBoard.style.backgroundImage = `url('../images/backGround${this.id}.jpg')`;
-  // Method 1 of changing puzzle
-  //puzzlePieces[0].src = `images/topLeft${event.currentTarget.id}.jpg`;
-  //puzzlePieces[1].src = `images/topRight${event.currentTarget.id}.jpg`;
-  //puzzlePieces[2].src = `images/bottomLeft${event.currentTarget.id}.jpg`;
-  //puzzlePieces[3].src = `images/bottomRight${event.currentTarget.id}.jpg`;
+// Save the original positions of instruments
+instruments.forEach(instrument => {
+  originalPositions.push(instrument.parentElement);
+});
 
-  // Method 2 of changing puzzle using foreach loop and put back puzzle
-  const pos = ["topLeft", "topRight", "bottomLeft", "bottomRight"];
-
-  puzzlePieces.forEach((piece, index) => {
-    piece.src = `images/${pos[index]}${event.currentTarget.id}.jpg`;
-    puzzlePieceDiv.appendChild(piece);
+// Functions
+function playAudio() {
+  playingAudios.forEach(audio => {
+    audio.play();
   });
+}
 
-  //Method 2
-  console.log(event.currentTarget.id);
-  puzzleBoard.style.backgroundImage = `url('../images/backGround${event.currentTarget.id}.jpg')`;
+function pauseAudio() {
+  playingAudios.forEach(audio => {
+    audio.pause();
+  });
+}
+
+function restartAudio() {
+  playingAudios.forEach(audio => {
+    audio.currentTime = 0;
+    audio.play();
+  });
+}
+
+function setVolume() {
+  playingAudios.forEach(audio => {
+    audio.volume = this.value / 100;
+  });
 }
 
 function handlestartDrag() {
@@ -45,23 +55,50 @@ function handleOver(e) {
 }
 
 function dropped() {
-  // Method 1 check is there any image before the first puzzle drop
-  // if (this.children.length < 1) {
-  // this.appendChild(dragPiece);
-  //}
-
-  //Method 2 check is there any image after the first puzzle drop
   if (this.children.length >= 1) {
     return;
   }
   this.appendChild(dragPiece);
+
+  // Restart and play all currently playing audios
+  playingAudios.forEach(audio => {
+    audio.currentTime = 0;
+    audio.play();
+  });
+
+  // Play corresponding audio
+  const trackId = dragPiece.getAttribute("data-track");
+  const audioElement = document.getElementById(trackId);
+  
+  if (audioElement) {
+    if (!playingAudios.includes(audioElement)) {
+      playingAudios.push(audioElement);
+    }
+    audioElement.currentTime = 0;
+    audioElement.play();
+  }
 }
 
-//event
+function resetPositions() {
+  instruments.forEach((instrument, index) => {
+    // Move instrument back to its original position
+    originalPositions[index].appendChild(instrument);
+  });
 
-theButtons.forEach((button) => button.addEventListener("click", changeBGImage));
-puzzlePieces.forEach((piece) =>
-  piece.addEventListener("dragstart", handlestartDrag)
-);
+  // Stop all playing audios and clear the playingAudios array
+  playingAudios.forEach(audio => {
+    audio.pause();
+    audio.currentTime = 0;
+  });
+  playingAudios = [];
+}
+
+// Events
+instruments.forEach((piece) => piece.addEventListener("dragstart", handlestartDrag));
 dropZones.forEach((zone) => zone.addEventListener("dragover", handleOver));
 dropZones.forEach((zone) => zone.addEventListener("drop", dropped));
+playButton.addEventListener("click", playAudio);
+pauseButton.addEventListener("click", pauseAudio);
+stopButton.addEventListener("click", restartAudio);
+resetButton.addEventListener("click", resetPositions);
+volSlider.addEventListener("change", setVolume);
